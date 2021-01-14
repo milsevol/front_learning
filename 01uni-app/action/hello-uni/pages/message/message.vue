9f284b6be301f1a2e9b6a19a159f09b5410aa2e9
小虫架构故事 <15013272917@163.com>
2021-01-14 22:26:28 +0800

    初始化uni-app项目和配置page页面属性

---
+++ 01uni-app/action/hello-uni/pages/message/message.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		这是信息页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
