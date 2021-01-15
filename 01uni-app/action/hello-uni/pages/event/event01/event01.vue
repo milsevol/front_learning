1a5f6afa73e0c1848fbeb081ac523d2881c8b9bc
小虫架构故事 <15013272917@163.com>
2021-01-15 23:20:41 +0800

    事件绑定

---
+++ 01uni-app/action/hello-uni/pages/event/event01/event01.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<button @click="tapHandle">点我啊</button>
+		<button @click="tapHandle2(1,$event)">点我啊</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			tapHandle(e) {
+				console.log(e)
+			},
+			tapHandle2(num, e) {
+				console.log(num, e)
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- 01uni-app/action/hello-uni/pages/index/index.vue
+++ 01uni-app/action/hello-uni/pages/index/index.vue
@@ -5,6 +5,7 @@
 		<button type="primary" @click="gotoPage('/pages/component/image01/image01')">图片</button>
 		<button type="primary" @click="gotoPage('/pages/component/style01/style01')">样式</button>
 		<button type="primary" @click="gotoPage('/pages/data/data01/data01')">数据绑定</button>
+		<button type="primary" @click="gotoPage('/pages/event/event01/event01')">事件绑定</button>
 	</view>
 </template>
 
